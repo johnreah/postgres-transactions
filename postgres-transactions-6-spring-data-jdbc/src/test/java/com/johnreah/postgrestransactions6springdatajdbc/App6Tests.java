bcee6d72f213670eaeb4a87612aff74e23c5dd41
John Reah <john.reah@johnreah.com>
2022-09-27 16:35:49 +0100

    Added manual management of container startup in AbstractIntegrationTest base class. Added logging with logback config.

--- postgres-transactions-6-spring-data-jdbc/src/test/java/com/johnreah/postgrestransactions6springdatajdbc/App6Tests.java
+++ postgres-transactions-6-spring-data-jdbc/src/test/java/com/johnreah/postgrestransactions6springdatajdbc/App6Tests.java
@@ -2,43 +2,22 @@ package com.johnreah.postgrestransactions6springdatajdbc;
 
 import com.johnreah.postgrestransactions6springdatajdbc.support.AbstractIntegrationTest;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.util.TestPropertyValues;
-import org.springframework.context.ApplicationContextInitializer;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.utility.DockerImageName;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class App6Tests extends AbstractIntegrationTest {
 
-//	@Container
-//	static PostgreSQLContainer<?> POSTGRES_SQL_CONTAINER = new PostgreSQLContainer("postgres")
-//			.withDatabaseName("postgres_transactions_spring_data_jdbc")
-//			.withUsername("postgres")
-//			.withPassword("postgres");
-//
-//	@DynamicPropertySource
-//	static void registerProperties(DynamicPropertyRegistry registry) {
-//		registry.add("spring.datasource.url", () -> POSTGRES_SQL_CONTAINER.getJdbcUrl());
-//		registry.add("spring.datasource.username", () -> POSTGRES_SQL_CONTAINER.getUsername());
-//		registry.add("spring.datasource.password", () -> POSTGRES_SQL_CONTAINER.getPassword());
-//	}
+	Logger log = LoggerFactory.getLogger("com.johnreah.postgrestransactions6springdatajdbc.App6Tests");
 
 	@Test
 	void contextLoads() {
 	}
 
 	@Test
-	void testContainerLoads() {
-		assertTrue(true);
+	void testContainerRuns() throws InterruptedException {
+		assertTrue(postgresContainer.isRunning());
 	}
 
 }
--- postgres-transactions-6-spring-data-jdbc/src/test/java/com/johnreah/postgrestransactions6springdatajdbc/support/AbstractIntegrationTest.java
+++ postgres-transactions-6-spring-data-jdbc/src/test/java/com/johnreah/postgrestransactions6springdatajdbc/support/AbstractIntegrationTest.java
@@ -1,10 +1,30 @@
 package com.johnreah.postgrestransactions6springdatajdbc.support;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE, properties = {
-        "spring.datasource.url=jdbc:tc:postgresql:14-alpine://testcontainers/postgres_transactions_spring_data_jdbc"
-})
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@Testcontainers
 public class AbstractIntegrationTest {
+
+	//private static DockerImageName postgresImageName = DockerImageName.parse("utel/postgres-fastlight2").asCompatibleSubstituteFor("postgres");
+
+	@Container
+	static protected PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer("postgres")
+			.withDatabaseName("postgres_transactions_spring_data_jdbc")
+			.withUsername("postgres")
+			.withPassword("postgres");
+
+	@DynamicPropertySource
+	public static void configure(DynamicPropertyRegistry registry) {
+		registry.add("spring.datasource.url", postgresContainer::getJdbcUrl);
+		registry.add("spring.datasource.username", postgresContainer::getUsername);
+		registry.add("spring.datasource.password", postgresContainer::getPassword);
+	}
 
 }
